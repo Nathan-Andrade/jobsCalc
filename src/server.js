c938b68e69c2c58f5aacb6c157dffdd7b6604597
Nathan de Andrade <62774392+Nathan-Andrade@users.noreply.github.com>
2021-03-29 22:55:45 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,4 +5,6 @@ const routes = require('./routes')
 server.use(express.static('public'))
 
 server.use(routes);
+
+//Servidor na Porta 3333
 server.listen(3333, () => console.log('Servidor rodando bem d++ :)'))
